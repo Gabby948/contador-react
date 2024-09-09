f14caa8026540c0fbec91e3a66a4a92ffafc9823
Gabby948 <teffa08-94@hotmail.com>
2024-09-09 18:17:06 -0500

    creando componente Contador. Close #1

---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Button from './Button';
+import './Counter.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="counter-container">
+      <h1>Contador con React</h1>
+      <p>{count}</p>
+      <div className="buttons-container">
+        <Button onClick={increment} text="Incrementar" />
+        <Button onClick={decrement} text="Decrementar" />
+        <Button onClick={reset} text="Reiniciar" />
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
